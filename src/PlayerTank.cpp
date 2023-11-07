9a2e442063dbb71dfc74dddf0449b8b53315c07f
Teemu Taivainen <teemu.taivainen@aalto.fi>
2023-11-07 22:02:16 +0200

    Add controls

--- src/PlayerTank.cpp
+++ src/PlayerTank.cpp
@@ -1,6 +1,6 @@
 #include "PlayerTank.hpp"
 
-PlayerTank::PlayerTank(sf::Vector2f initial_pos, int initial_speed) 
+PlayerTank::PlayerTank(sf::Vector2f initial_pos, sf::Vector2f initial_speed) 
   : pos(initial_pos), speed(initial_speed) {
       tankShape.setOrigin(50, 50);
       tankShape.setSize(sf::Vector2f(100,100));
@@ -8,10 +8,27 @@ PlayerTank::PlayerTank(sf::Vector2f initial_pos, int initial_speed)
   }
 
 void PlayerTank::UpdatePos() {
-  pos += sf::Vector2f(speed, speed);
+  if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+    pos.x -= speed.x; 
+  }
+  if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+    pos.x += speed.x; 
+  }
+  if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+    pos.y -= speed.y; 
+  }
+  if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+    pos.y += speed.y; 
+  }
+  tankShape.setPosition(pos);
 }
 
 void PlayerTank::Draw(sf::RenderWindow &window) const {
   window.draw(tankShape);
+}
+
+void PlayerTank::UpdatePosAndDraw(sf::RenderWindow &window) {
+  UpdatePos();
+  Draw(window);
 }
 
--- src/PlayerTank.hpp
+++ src/PlayerTank.hpp
@@ -2,13 +2,14 @@
 
 class PlayerTank {
   public:
-    PlayerTank(sf::Vector2f initial_pos, int initial_speed);
+    PlayerTank(sf::Vector2f initial_pos, sf::Vector2f initial_speed);
 
     void UpdatePos();
     void Draw(sf::RenderWindow &window) const ;
+    void UpdatePosAndDraw(sf::RenderWindow &window);
 
   private:
     sf::Vector2f pos;
-    int speed;
+    sf::Vector2f speed;
     sf::RectangleShape tankShape;
 };
