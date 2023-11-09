6251e05a687e4377bdfd8c9a4f4da7ea1cb6db59
Jaakko Pulkkinen <jaakko.pulkkinen@aalto.fi>
2023-11-09 17:07:23 +0200

    Not working textures

--- src/Tank.cpp
+++ src/Tank.cpp
@@ -1,4 +1,7 @@
 #include "include/Tank.hpp"
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Image.hpp>
 
 Tank::Tank(sf::Vector2f initial_pos, sf::Vector2f initial_speed) 
   : speed_(initial_speed), cooldown_timer_(0) {
@@ -7,9 +10,22 @@ Tank::Tank(sf::Vector2f initial_pos, sf::Vector2f initial_speed)
       tank_shape_.setPosition(initial_pos);
   }
 
+void TextureConstruct(sf::Vector2f pos){
+  sf::Texture textureNoPiippu;
+    if(!textureNoPiippu.loadFromFile("C://TankNoPiippu.png")){
+  std::cout << "No can do";
+  }
+  sf::Sprite tankNoPiippu;
+  tankNoPiippu.setTexture(textureNoPiippu);
+  tankNoPiippu.setPosition(pos);
+}
+
+
 void Tank::Draw(sf::RenderWindow &window) const {
   window.draw(tank_shape_);
+  TextureConstruct(tank_shape_.getPosition());
 }
+
 
 void Tank::Shoot(std::vector<Projectile> &projectiles, float angle) {
   Projectile new_projectile(tank_shape_.getPosition(), 3, angle);
