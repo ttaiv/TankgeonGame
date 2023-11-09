7d374f45ba3964bc9d8431535a291e2d6ff99b45
Janne Kantola <janne.kantola@aalto.fi>
2023-11-09 13:05:45 +0200

    Implement Wall class and draw some in main

--- src/include/Wall.hpp
+++ src/include/Wall.hpp
@@ -4,9 +4,39 @@
 class Wall: public Obstacle 
 {
   public:
-    virtual bool IsProjectilePassable() {return false;}
-    virtual bool IsTankPassable() {return false;}
     
+    /**
+     * @brief Construct a new Wall object
+     * 
+     * @param position 
+     * @param size 
+     */
+    Wall(sf::Vector2f position, sf::Vector2f size);
 
+    /**
+     * @brief Tells that a projectile cannot pass a wall
+     *  
+     * @return false 
+     */
+    virtual bool IsProjectilePassable();
+    
+    
+    /**
+     * @brief Tells that tank cannot pass a wall
+     * 
+     * @return false 
+     */
+    virtual bool IsTankPassable();
+
+    /**
+     * @brief Draws a wall to window given as a parameter.
+     * 
+     * @param window 
+     */
+    virtual void Draw(sf::RenderWindow &window) const override;
+    
   private:
+    sf::Vector2f position_;
+    sf::Vector2f size_;
+    sf::RectangleShape wallShape_;
 };
