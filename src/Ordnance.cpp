062fb24db366e768a58595683d6ad80340075937
Teemu Taivainen <teemu.taivainen@aalto.fi>
2023-11-15 20:49:52 +0200

    Yeeted ordnance

--- src/Ordnance.cpp
+++
@@ -1,9 +0,0 @@
-#include "include/Ordnance.hpp"
-
-// Ordnance::Ordnance(sf::Vector2f initial_position) : pos_(initial_position) {}
-
-Ordnance::Ordnance() : is_active_(true) {}
-
-bool Ordnance::IsActive() const { return is_active_; }
-void Ordnance::SetInactive() { is_active_ = false; }
-void Ordnance::SetActive() { is_active_ = true; }
