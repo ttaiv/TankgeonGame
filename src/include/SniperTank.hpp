228381c125264cc814bb431a0f4adc794625863d
Teemu Taivainen <teemu.taivainen@aalto.fi>
2023-12-01 23:50:54 +0200

    Adjust sniper tank

--- src/include/SniperTank.hpp
+++ src/include/SniperTank.hpp
@@ -17,7 +17,8 @@ class SniperTank : public EnemyTank {
     enum MovementOption {
       TurnLeft = 0,
       TurnRight,
-      GoForward,
+      GoForward1,
+      GoForward2,
       GoBack,
       MovementCount // Count enum values
     };
@@ -26,5 +27,5 @@ class SniperTank : public EnemyTank {
 
     std::uniform_int_distribution<int> movement_dis_{0, MovementCount - 1};
     int movement_timer_ = 0;
-    MovementOption current_movement_ = MovementOption::GoForward;
+    MovementOption current_movement_ = MovementOption::GoForward1;
 };
