5ec6adf66536a80b6a4fe697c7413e753944be5b
Teemu Taivainen <teemu.taivainen@aalto.fi>
2023-12-05 22:22:42 +0200

    Add comments to level

--- src/include/Level.hpp
+++ src/include/Level.hpp
@@ -17,6 +17,10 @@
 #include <string>
 #include <fstream>
 
+/**
+ * @brief Struct that contains level data. Used to pass level data for tanks.
+ * 
+ */
 struct LevelData {
   std::vector<Wall> walls;
   std::vector<Spike> spikes;
@@ -25,7 +29,10 @@ struct LevelData {
   std::list<Shield> shields;
 };
 
-
+/**
+ * @brief Class that represents a level in the game. When necessary, loads level data from a text file.
+ * 
+ */
 class Level {
   public:
     static constexpr int LEVEL_FILE_WIDTH = 32;
@@ -43,11 +50,31 @@ class Level {
      * @param window_size 
      */
     void LoadFromFile(int level_number, sf::Vector2u window_size);
+    /**
+     * @brief Updates the level by updating all tanks and projectiles. Also handles collisions.
+     * 
+     * @param window that displays the game
+     */
     void UpdateLevel(sf::RenderWindow &window);
+    /**
+     * @brief Draws the level by drawing all tanks, projectiles, and walls.
+     * 
+     * @param window that displays the game
+     */
     void DrawLevel(sf::RenderWindow &window);
+    /**
+     * @brief Checks if the level is completed. A level is completed if all enemies are destroyed.
+     * 
+     * @return true if the level is completed
+     * @return false if the level is not completed
+     */
     bool IsCompleted() const;
+    /**
+     * @brief Getter function that returns the level data.
+     * 
+     * @return const LevelData& 
+     */
     const LevelData& GetLevelData();
-    
     /**
      * @brief Getter function that returns the player tank.
      * 
@@ -64,22 +91,37 @@ class Level {
     sf::Texture background_texture_;
     sf::Sprite background_;
 
+    /**
+     * @brief Checks if projectiles collide with anything. If a collision is detected then the correct actions are executed.
+     * 
+     */
     void HandleProjectileCollisions();
-    
-/**
- * @brief Handles item pick ups by checking if the player collides with some item.
- * If a collision with an item is detected then the correct actions are executed
- * depending on the item type.
- */
+    /**
+     * @brief Handles item pick ups by checking if the player collides with some item.
+     * If a collision with an item is detected then the correct actions are executed
+     * depending on the item type.
+     */
     void HandleItemPickUps();
-
+    /**
+     * @brief Fills two dimensional vector with level data from a text file.
+     * 
+     * @param level_grid 
+     * @param filepath 
+     */
     void FillGridFromFile(std::vector<std::string> &level_grid, const std::string &filepath);
-
+    /**
+     * @brief Creates boundary walls for the level.
+     * 
+     * @param window_size 
+     */
     void SetBorderWalls(sf::Vector2u window_size);
-
+    /**
+     * @brief Clears all level data.
+     * 
+     */
     void Clear();
 
-
+    // Not needed in current implementation
     sf::Vector2u CountNeighboringObstacles(const std::vector<std::string> &level_grid,
       std::array<std::array<bool, LEVEL_FILE_WIDTH>, LEVEL_FILE_HEIGHT> &visited, int start_x, int start_y, char obstacle);
 };
